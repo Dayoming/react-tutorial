88d38ec9d5046cc618325d07f29ce56d6c397fed
Dayoming <jdayoung1204@naver.com>
2022-05-16 21:39:42 +0900

    3.3.1~2 컴포넌트 사용할 때 props 값 지정하기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,8 @@
-import { Component } from "react";
 import MyComponent from "./MyComponent.js";
 import "./App.css";
 
 const App = () => {
-  return <MyComponent />;
+  return <MyComponent name="React" />;
 };
 
 export default App;
