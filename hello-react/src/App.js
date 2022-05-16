fbf619aac02f1429d27a03f506bc7525f759a9e3
Dayoming <jdayoung1204@naver.com>
2022-05-16 19:59:48 +0900

    3.2.3 모듈 내보내기 및 불러오기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,9 @@
 import { Component } from "react";
+import MyComponent from "./MyComponent.js";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    const name = "react";
-    return <div className="react">{name}</div>;
-  }
-}
+const App = () => {
+  return <MyComponent />;
+};
 
 export default App;
